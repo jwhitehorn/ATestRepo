02b7a0a42f2d2c73571eafff84d77254dc999ce4
Jason Whitehorn <jason@whitehorn.us>
2021-09-01 21:22:28 -0500

    Create runTests.sh

---
+++ scripts/runTests.sh
@@ -0,0 +1,18 @@
+curl -L https://get.pharo.org/64/ | bash
+./pharo --headless Pharo.image ./scripts/runTests.st
+
+FILE=/tmp/result.txt
+if [ ! -f "$FILE" ]; then
+    echo "ERROR: $FILE does not exists!"
+    exit 1
+fi
+
+# Print the result of the tests
+cat $FILE
+if grep -q ERROR "$FILE"; then
+    echo "SOME ERRORS!"
+    exit 1
+else
+    echo "ALL TEST PASSED"
+    exit 0
+fi
